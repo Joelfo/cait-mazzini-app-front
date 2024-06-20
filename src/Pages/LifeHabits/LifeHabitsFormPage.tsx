635514a43f8f40c088c01bf8006b3519f0e9388f
Joel Lopes <joellopessouza@id.uff.br>
2024-06-19 22:14:53 -0300

    Dockerfile

--- src/Pages/LifeHabits/LifeHabitsFormPage.tsx
+++ src/Pages/LifeHabits/LifeHabitsFormPage.tsx
@@ -1,16 +1,11 @@
-import { ContraceptiveMethodAPI, useContraceptiveMethodApi } from "Api/useContraceptiveMethodApi";
-import { LifeHabitsInfoAPI, useLifeHabitsInfoApi } from "Api/useLifeHabitsInfoApi";
-import { PhysicalActivityAPI, usePhysicalActivityApi } from "Api/usePhysicalActivityApi";
-import { ContraceptiveMethodForm } from "Components/Forms/ContraceptiveMethodForm";
-import { PhysicalActivityForm } from "Components/Forms/PhysicalActivityForm";
+import { useContraceptiveMethodApi } from "Api/useContraceptiveMethodApi";
+import { useLifeHabitsInfoApi } from "Api/useLifeHabitsInfoApi";
+import { usePhysicalActivityApi } from "Api/usePhysicalActivityApi";
 import { LifeHabitsForm } from "Components/LifeHabitsForm/LifeHabitsForm";
-import { MazziniPopup } from "Components/MazziniPopup/MazziniPopup";
 import { ConnectionErrorAlert } from "Components/Utils/Alert/ConnectionErrorAlert";
 import { useSelectedPatient } from "Hooks/useSelectedPatient"
 import { useEffect, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { ContraceptiveMethod } from "Api/Types/ContraceptiveMethod";
-import { PhysicalActivity } from "Api/Types/PhysicalActivity";
 
 export const LifeHabitsFormPage = () => {
     // useEffect
